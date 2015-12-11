ed3957bcad99cd039ddcde40d44f0502ba15d371
khoulahan <houlahanke@gmail.com>
2015-12-11 10:27:57 -0800

    Initial commit of create_literature_variants_dotmap.R and implemented pathway incorporation when extracting variants

---
+++ R/create_literature_variants_dotmap.R
@@ -0,0 +1,262 @@
+### create_literature_variants_dotmap.R ###########################################################
+# pulls out all significant variant-compound combinations and plots as a dotmap
+## dot size and colour indicates effect size and direction 
+## background shading indicates FDR
+## covariates indicating if result is in tissue reported in literature or non-reported tissues
+## covariates also show compound analyzed
+library(BoutrosLab.plotting.general);
+###################################################################################################
+create.literature.variants.dotmap <- function(
+	hypothesis.results, 
+	map.drug.names, 
+	filename, 
+	dataset = 'CCLE', 
+	width = 5, 
+	height = 6,
+	x.legend = -0.2,
+	y.legend = -0.28,
+	spot.size.function = function(x) { 0.5 + (1.2 * abs(x)); }
+	) {
+
+		# add dataset column 
+		hypothesis.results$dataset <- dataset;
+		
+		### MAKE DATASETS CONSISTENT - MATCH EFFECT SIZE SIGN AND COMPOUND NAMES ###
+		if (dataset == 'CCLE') {
+			# change CCLE effect sizes sign to match CTDD and CGP
+			hypothesis.results[hypothesis.results$dataset == 'CCLE','effect.size'] <- hypothesis.results[hypothesis.results$dataset == 'CCLE','effect.size']*(-1)
+		} else if (dataset == 'CTDD') {
+			# change all drug names to match CCLE
+			hypothesis.results$compound <- unlist(sapply(hypothesis.results$compound, function(x) {
+				map.drug.names[map.drug.names$CTDD == x & !is.na(map.drug.names$CTDD),'CCLE'];
+				}))
+		} else if (dataset == 'CGP') {
+			# change all drug names to match CCLE
+			hypothesis.results$compound <- unlist(sapply(hypothesis.results$compound, function(x) {
+				map.drug.names[map.drug.names$CGP == x & !is.na(map.drug.names$CGP),'CGP']
+				}))
+		}
+
+		### CORRECT P-VALUES & PULL OUT SIGNIFICANT VARIANTS ###
+		# adjust pvalues
+		hypothesis.results$p.value <- p.adjust(hypothesis.results$p.value, method = 'fdr')
+		# only variants with significant results
+		sig.variant.drug <- by(
+			hypothesis.results,
+			hypothesis.results[,c('variant','compound')],
+			function(subset) {
+				if (any(subset$p.value < 0.05)) {
+					return(data.frame(variant = unique(subset$variant), drug = unique(subset$compound)))
+				} else {
+					return(NULL)
+				}
+			});
+		sig.variant.drug <- do.call(rbind, sig.variant.drug)
+		# filter hypothesis results to keep only significant compounds and variants
+		hypothesis.results <- hypothesis.results[hypothesis.results$variant %in% sig.variant.drug$variant & hypothesis.results$compound %in% sig.variant.drug$drug,];
+
+		### FORMAT DATA FOR PLOTTING ###
+		# create identifying group column
+		hypothesis.results$group <- apply(
+			hypothesis.results,
+			1,
+			function(x) {
+				paste(x['compound'],x['dataset'],x['tissue'], sep = '_')
+				})
+		# factor variant and group columns
+		hypothesis.results$variant 	<- factor(hypothesis.results$variant);
+		hypothesis.results$group 	<- factor(hypothesis.results$group);
+		# reformat p-value, num cell lines and effect size
+		reformat.data.frames <- function(hypothesis.results, variable) {
+			tmp <- matrix(
+				nrow=nlevels(hypothesis.results$group), 
+				ncol=nlevels(hypothesis.results$variant),
+				dimnames=list(levels(hypothesis.results$group), levels(hypothesis.results$variant))
+				)
+			tmp[cbind(hypothesis.results$group, hypothesis.results$variant)] <- hypothesis.results[,variable]
+			tmp
+		}
+		pvalues <- reformat.data.frames(hypothesis.results,'p.value');
+		effect.size <- reformat.data.frames(hypothesis.results,'effect.size');
+
+		### ORDER PLOTTING DATA ###
+		# order by pvalues
+		max.effect.size <- apply(effect.size,2, function(x) {max(abs(x), na.rm = TRUE)});
+		effect.size <- effect.size[,order(
+			colSums(-log10(pvalues), na.rm = TRUE),
+			max.effect.size,
+			decreasing = TRUE)];
+		pvalues <- pvalues[,order(
+			colSums(-log10(pvalues), na.rm = TRUE), 
+			max.effect.size,
+			decreasing = TRUE)];
+		# order compounds as in map drug names file so that similar mode of actions are adjacent
+		effect.size <- effect.size[rownames(effect.size) %in% map.drug.names$CCLE,];
+		pvalues 	<- pvalues[rownames(effect.size) %in% map.drug.names$CCLE,];
+
+		### SPOT COLOUR FUNCTION ###
+		spot.colour.function <- function(x) {
+			colours <- rep('white', length(x));
+			colours[sign(x) ==  1] <- 'dodgerblue2';
+			colours[sign(x) == -1] <- 'firebrick2';
+			return(colours);
+			}
+
+		### GENERATE COVARIATES ###
+		# tissue covariate colours
+		tissue.colours <- rownames(pvalues);
+		tissue.colours[grep('NonAssociated', tissue.colours)] <- default.colours(2, palette.type = 'spiral.sunrise')[1];
+		tissue.colours[grep('Associated', tissue.colours)] <- default.colours(2, palette.type = 'spiral.sunrise')[2];
+		# compound covariate colours
+		compound.colours <- rownames(pvalues);
+		for (i in 1:nrow(map.drug.names)) {
+			compounds.used[i] <- any(grepl(map.drug.names[i,'CCLE'], compound.colours));
+			compound.colours[grep(map.drug.names[i,'CCLE'], compound.colours)] <- map.drug.names[i,'Colour'];
+		}
+		# create covariates
+		covariate <- list(
+			rect = list(
+				col = 'black',
+				fill = tissue.colours,
+				lwd = 1.5
+				),
+			rect = list(
+				col = 'black',
+				fill = compound.colours,
+				lwd = 1.5
+				)
+			);
+		cov.grob <- covariates.grob(
+			covariates = covariate,
+			ord = c(length(tissue.colours):1),
+			side = 'right'
+			);
+
+		### GENERATE COVARIATE LEGEND ###
+		cov.legend <- list(
+			legend = list(
+				colours = default.colours(2, palette.type = 'spiral.sunrise'),
+				labels = c('Non-Associated','Associated'),
+				cex = 1,
+				title = 'Tissue'
+				),
+			legend = list(
+				colours = map.drug.names[compounds.used,'Colours'],
+				labels = map.drug.names[compounds.used,'CCLE'],
+				cex = 1,
+				title = 'Compounds'
+				)
+			);
+
+		cov.legend <- legend.grob(
+			legends = cov.legend,
+			title.cex = 0.8,
+			label.cex = 0.75,
+			size = 1
+			);
+
+		### CREATE GRID LAYOUT FOR COVARIATES AND LEGEND ###
+		right.layout <- grid.layout(
+			nrow = 1,
+			ncol = 2,
+			width = unit(
+				x = c(0,1),
+				units = rep('lines',2)
+				),
+			heights = unit(
+				x = c(1,1),
+				units = rep('npc', 1)
+				)
+			);
+		right.grob <- frameGrob(layout = right.layout);
+		right.grob <- packGrob(
+			frame = right.grob,
+			grob = cov.grob,
+			row = 1,
+			col = 1
+			);
+		right.grob <- packGrob(
+			frame = right.grob,
+			grob = cov.legend,
+			row = 1,
+			col = 2
+		 	);
+
+		### CREATE DOT LEGEND ###
+		key.sizes <- c(1.5, 1,0.5,-0.5,-1,-1.5)
+		dot.grob <- draw.key(
+			list(
+				space = 'right',
+				points = list(
+					cex = spot.size.function(key.sizes),
+					col = 'black',
+					pch = 21,
+					fill = spot.colour.function(key.sizes)
+					),
+				text = list(
+					lab = c("+1.5","+1.0","+0.5","-0.5","-1.0","-1.5"),
+					cex = 0.75,
+					adj = 1
+					),
+				title = 'Effect Size',
+				cex.title = 0.8,
+				background = 'white',
+				padding.text = 3
+				)
+			);
+
+		### CREATE DOTMAP ###
+		create.dotmap(
+			x = effect.size,
+			bg.data = -log10(pvalues),
+			filename = filename,
+			spot.colour.function = spot.colour.function,
+			spot.size.function = spot.size.function,
+			pch = 21,
+			bg.alpha = 1,
+			yaxis.lab = rep('', nrow(pvalues)),
+			colour.scheme = c('white','black'),
+			colourkey = TRUE,
+			yaxis.cex = 0.5,
+			xaxis.cex = 1,
+			xaxis.rot = 90,
+			NA.spot.size = 1.5,
+			resolution = 500,
+			at = seq(0,5,1),
+			colourkey.cex = 0.9, 
+			colourkey.labels.at = seq(0,5,1),
+			colourkey.labels = c(
+				expression(10^0),
+				expression(10^-1),
+				expression(10^-2),
+				expression(10^-3),
+				expression(10^-4),
+				substitute(p.value<=10^-5, list(p.value = ''))
+				),
+			legend = list(
+				right = list(       
+					fun = right.grob
+					),
+				left = list(
+					fun = dot.grob
+					),
+				inside = list(
+					fun = draw.key(
+						key = list(
+							text = list(
+								lab = 'FDR:',
+								cex = 1,
+								adj = 1
+								)
+					        )
+						),
+					x = x.legend,
+					y = y
+					)
+				),
+			axis.bottom = 1.05,
+			bottom.padding = 4,
+			height = height,
+			width = width
+			)
+	}
--- R/extract_biomarkers_drug_response.R
+++ R/extract_biomarkers_drug_response.R
@@ -7,7 +7,8 @@ extract.biomarkers.drug.response <- function(
 	compound,
 	map.tissue,
 	dataset = 'CCLE',
-	variant.type = 'mutation'
+	variant.type = 'mutation',
+	additional.variant = NULL
 	) {
 
 		# find all variants associated with specified compound
@@ -20,77 +21,94 @@ extract.biomarkers.drug.response <- function(
 			variant.type = variant.type
 			);
 
+		# add additional variants to look at, if specified
+		if (!is.null(additional.variant)) {
+			for (variant in additional.variant) {
+				if (variant %in% associated.biomarkers.variants$Gene) {
+					next;
+				} else {
+					tmp <- data.frame(Disease = 'none', Gene = variant, Variant = NA, Association = 'none', Therapeutic.context = compound)
+					associated.biomarkers.variants <- rbind(associated.biomarkers.variants, tmp);
+				}
+			}
+		}
+
 		# for each gene find tissues that associated with listed diseases and pull out drug response for all those tissues	
 		drug.response.variants <- by(
 			associated.biomarkers.variants,
 			associated.biomarkers.variants$Gene,
 			function(x) {
-				tissues <- unique(map.tissue[map.tissue$disease %in% x[,'Disease'],grep(dataset, colnames(map.tissue))]);
 				gene <- as.character(unique(x$Gene));
-				variant.cat <- max(as.character(x$Variant));
-				# check that gene is in mutations if not move onto next variant
-				if (variant.type == 'mutation' & !gene %in% colnames(mutations) | variant.type == 'cnv' & !gene %in% colnames(cnv)) {
-					return(NULL);
-				}
-				# get drug reponse of all tissues associated with gene of interest
-				associated.drug.response <- sapply(
-					tissues,
-					function(tissue) {
-						if (variant.type == 'mutation') {
-							# find cell lines associated with tissue that have variant
-							cell.lines.variant <- rownames(mutations[mutations[,gene] == 1,])[grep(tissue, 
-								rownames(mutations[mutations[,gene] == 1,]), ignore.case = TRUE)];
-						} else if (variant.type == 'cnv') {
-							# find cell lines with amplification or deletion 
-							copy.number.variant <- round(2*(2^cnv[,gene]));
-							if (variant.cat == 'amplification') {
-								cell.lines.variant <- rownames(cnv)[which(copy.number.variant > 2)];
-							} else if (variant.cat == 'deletion') {
-								cell.lines.variant <- rownames(cnv)[which(copy.number.variant < 2)];
+				if (all(x[,'Disease'] != 'none')) {
+					tissues <- unique(map.tissue[map.tissue$disease %in% x[,'Disease'],grep(dataset, colnames(map.tissue))]);
+					variant.cat <- max(as.character(x$Variant));
+					# check that gene is in mutations if not move onto next variant
+					if (variant.type == 'mutation' & !gene %in% colnames(mutations) | variant.type == 'cnv' & !gene %in% colnames(cnv)) {
+						return(NULL);
+					}
+					# get drug reponse of all tissues associated with gene of interest
+					associated.drug.response <- sapply(
+						tissues,
+						function(tissue) {
+							if (variant.type == 'mutation') {
+								# find cell lines associated with tissue that have variant
+								cell.lines.variant <- rownames(mutations[mutations[,gene] == 1,])[grep(tissue, 
+									rownames(mutations[mutations[,gene] == 1,]), ignore.case = TRUE)];
+							} else if (variant.type == 'cnv') {
+								# find cell lines with amplification or deletion 
+								copy.number.variant <- round(2*(2^cnv[,gene]));
+								if (variant.cat == 'amplification') {
+									cell.lines.variant <- rownames(cnv)[which(copy.number.variant > 2)];
+								} else if (variant.cat == 'deletion') {
+									cell.lines.variant <- rownames(cnv)[which(copy.number.variant < 2)];
+								}
+								cell.lines.variant <- cell.lines.variant[grep(tissue, cell.lines.variant, ignore.case = TRUE)];
 							}
-							cell.lines.variant <- cell.lines.variant[grep(tissue, cell.lines.variant, ignore.case = TRUE)];
-						}
-						# only keep cell lines that have drug response
-						cell.lines.variant <- cell.lines.variant[cell.lines.variant %in% rownames(drug.response)];
-						# find cell lines associated with tissue that do not have variant
-						cell.lines.no.variant <- rownames(drug.response)[grep(tissue, rownames(drug.response), ignore.case = TRUE)];
-						cell.lines.no.variant <- cell.lines.no.variant[!cell.lines.no.variant %in% cell.lines.variant];
+							# only keep cell lines that have drug response
+							cell.lines.variant <- cell.lines.variant[cell.lines.variant %in% rownames(drug.response)];
+							# find cell lines associated with tissue that do not have variant
+							cell.lines.no.variant <- rownames(drug.response)[grep(tissue, rownames(drug.response), ignore.case = TRUE)];
+							cell.lines.no.variant <- cell.lines.no.variant[!cell.lines.no.variant %in% cell.lines.variant];
 
-						# find the most predominant therapeutic association 
-						therapeutic.association <- summary(
-							x[x$Disease %in% map.tissue[map.tissue[,grep(dataset, colnames(map.tissue))] == tissue,'disease'],'Association']
-							);
-						therapeutic.association <- names(therapeutic.association)[therapeutic.association == max(therapeutic.association)][1];
+							# find the most predominant therapeutic association 
+							therapeutic.association <- summary(
+								x[x$Disease %in% map.tissue[map.tissue[,grep(dataset, colnames(map.tissue))] == tissue,'disease'],'Association']
+								);
+							therapeutic.association <- names(therapeutic.association)[therapeutic.association == max(therapeutic.association)][1];
 
-						data.frame(
-							drug.response = c(
-								drug.response[cell.lines.variant,compound],
-								drug.response[cell.lines.no.variant,compound]
-								),
-							cell.line = c(
-								cell.lines.variant,
-								cell.lines.no.variant
-								),
-							tissue = rep(tissue, length(c(cell.lines.variant, cell.lines.no.variant))),
-							gene = rep(gene, length(c(cell.lines.variant, cell.lines.no.variant))),
-							variant = c(
-								rep('Variant', length(cell.lines.variant)),
-								rep('NoVariant', length(cell.lines.no.variant))
-								),
-							association = c(
-								rep(therapeutic.association, length(cell.lines.variant)),
-								rep('none', length(cell.lines.no.variant))
-								)
-							);
-						},
-					simplify = FALSE
-					);
-				associated.drug.response <- do.call(rbind, associated.drug.response);
-				# add the remaining tissues binned as "other"
-				associated.tissues <- paste(tissues, collapse = '|');
-				# find all tissues not associated with variants
-				non.associated.tissues <- rownames(drug.response)[grep(associated.tissues, rownames(drug.response), 
-					invert = TRUE, ignore.case = TRUE)];
+							data.frame(
+								drug.response = c(
+									drug.response[cell.lines.variant,compound],
+									drug.response[cell.lines.no.variant,compound]
+									),
+								cell.line = c(
+									cell.lines.variant,
+									cell.lines.no.variant
+									),
+								tissue = rep(tissue, length(c(cell.lines.variant, cell.lines.no.variant))),
+								gene = rep(gene, length(c(cell.lines.variant, cell.lines.no.variant))),
+								variant = c(
+									rep('Variant', length(cell.lines.variant)),
+									rep('NoVariant', length(cell.lines.no.variant))
+									),
+								association = c(
+									rep(therapeutic.association, length(cell.lines.variant)),
+									rep('none', length(cell.lines.no.variant))
+									)
+								);
+							},
+						simplify = FALSE
+						);
+					associated.drug.response <- do.call(rbind, associated.drug.response);
+					# add the remaining tissues binned as "other"
+					associated.tissues <- paste(tissues, collapse = '|');
+					# find all tissues not associated with variants
+					non.associated.tissues <- rownames(drug.response)[grep(associated.tissues, rownames(drug.response), 
+						invert = TRUE, ignore.case = TRUE)];
+					} else {
+						# if disease == 'none' all cell lines are non associated tissues
+						non.associated.tissues <- rownames(drug.response)
+					}
 				# find non associated cell lines with variant
 				if (variant.type == 'mutation') {
 					# find cell lines not associated with tissue that have variant
@@ -129,7 +147,11 @@ extract.biomarkers.drug.response <- function(
 					association = rep('none', length(c(cell.lines.variant, cell.lines.no.variant)))
 					);
 
-				return(rbind(associated.drug.response, non.associated.drug.response));
+				if (all(x[,'Disease'] != 'none')) {
+					return(rbind(associated.drug.response, non.associated.drug.response));
+					} else {
+						return(non.associated.drug.response);
+					}
 				}
 			);
 		drug.response.variants <- do.call(rbind, drug.response.variants);
