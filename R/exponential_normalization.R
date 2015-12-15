abfc275e7bbefc388e3f3d2555e90117bdc3c15f
khoulahan <houlahanke@gmail.com>
2015-12-15 14:32:51 -0800

    Initial commit of exponential_normalization.R

---
+++ R/exponential_normalization.R
@@ -0,0 +1,17 @@
+### exponential_normalization.R ###################################################################
+# normalizes gene expression data based on underlying exponential distribution 
+###################################################################################################
+exponential.normalization <- function(gene.expression) {
+	# sample-wise normalization 
+	normalized.gene.expression <- apply(
+		gene.expression,
+		1,
+		function(sample) {
+			sample <- rank(sample);
+			sample <- max(sample) - sample;
+			sample <- (sample-min(sample))/(max(sample)-min(sample));
+			dexp(sample);
+			}
+		);
+	return(normalized.gene.expression);
+}
